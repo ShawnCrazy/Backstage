11bf329f2a58c40bf8877aea506aa4ad68f8bc5c
ShawnCrazy <2273904129@qq.com>
2017-11-06 18:49:55 +0800

    添加了用于查询的model

---
+++ application/models/DBModel.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: njche
+ * Date: 2017/10/27
+ * Time: 16:23
+ */
+
+class DBModel extends CI_Model
+{
+    function __construct()
+    {
+        $this->load->database();
+    }
+
+    /*获取图片*/
+    function getImage($type)
+    {
+//        $sql = "select * from images WHERE image_type = 'slider'";
+//        $query = $this->db->query($sql);
+//        return $query->result_array();
+        $result = $this->db->select('*')
+            ->from('images')
+            ->where("image_type='$type'")
+            ->get();
+        return $result->result_array();
+    }
+    /*获取商品详情*/
+    function getInfo($type)
+    {
+        $result = $this->db->select('*')
+            ->from('goods')
+            ->where("image_type='$type'")
+            ->get();
+        return $result->result_array();
+    }
+    function getData($table, $type)
+    {
+        $result = $this->db->select('*')
+            ->from($table)
+            ->where("image_type='$type'")
+            ->get();
+        return $result->result_array();
+    }
+}
